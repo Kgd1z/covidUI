3aef48434470ab8270252999146b2d92aeaa3470
Casprine Assempah <casprine.001@gmail.com>
2020-04-19 19:44:54 +0000

    feat: updates after review

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,47 +1,28 @@
-import * as WebBrowser from "expo-web-browser";
-import * as React from "react";
-import {
-  Image,
-  Platform,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-  Button,
-  SafeAreaView,
-} from "react-native";
-import { ScrollView } from "react-native-gesture-handler";
-import { useNavigation } from "@react-navigation/native";
-import { MonoText } from "../components/StyledText";
+import * as React from 'react';
+import { Image, StyleSheet, Text, TouchableOpacity, View, SafeAreaView } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 export default function HomeScreen() {
   const navigation = useNavigation();
   return (
     <SafeAreaView style={styles.container}>
       <View>
-        <Image style={styles.img} source={require("../img/home.png")} />
+        <Image style={styles.img} source={require('../img/home.png')} />
       </View>
 
       <View style={styles.container}>
         <Text style={styles.heading}>Find What You</Text>
         <Text style={styles.heading}>Need Service Covid 19</Text>
-        <Text style={styles.content}>
-          Find out everything there is to know about covid 19.
-        </Text>
+        <Text style={styles.content}>Find out everything there is to know about covid 19. Lorem, ipsum dolor sit</Text>
 
         <TouchableOpacity
           style={styles.button}
           onPress={() => {
-            navigation.navigate("LinksScreen");
+            navigation.navigate('LinksScreen');
           }}
         >
-          <Text style={{ fontWeight: "bold", marginLeft: 10 }}>
-            Get Started
-          </Text>
-          <Image
-            source={require("../img/next.png")}
-            style={{ width: 20, height: 20, marginLeft: 150 }}
-          />
+          <Text style={{ fontWeight: 'bold', marginLeft: 10 }}>Get Started</Text>
+          <Image source={require('../img/next.png')} style={{ width: 20, height: 20, marginLeft: 150 }} />
         </TouchableOpacity>
       </View>
     </SafeAreaView>
@@ -51,32 +32,32 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#0379FC",
+    backgroundColor: '#0379FC',
     padding: 20,
   },
   button: {
-    backgroundColor: "#fff",
+    backgroundColor: '#fff',
     height: 52,
-    alignItems: "center",
+    alignItems: 'center',
     padding: 15,
     marginBottom: 20,
-    marginTop: 20,
+    marginTop: 'auto',
     marginLeft: 40,
     marginRight: 20,
     width: 300,
-    flexDirection: "row",
+    flexDirection: 'row',
   },
   heading: {
-    color: "#fff",
+    color: '#fff',
     fontSize: 30,
-    fontWeight: "bold",
-    textAlign: "center",
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
   content: {
     marginTop: 20,
     fontSize: 15,
-    color: "#BDC4c6",
-    textAlign: "center",
+    color: '#CBD5E0',
+    textAlign: 'center',
     marginBottom: 90,
   },
   img: {
